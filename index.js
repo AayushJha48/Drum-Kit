ada2ce92885a6f778adb9b8faa1a1718a1f75bd9
AayushJha48 <47394065+AayushJha48@users.noreply.github.com>
2019-03-05 15:19:54 +0530

    Initial Drum-Kit Website having some bugs

---
+++ index.js
@@ -0,0 +1,113 @@
+
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click" , function() {
+
+   var innerHtml = this.innerHTML;
+
+   switch (innerHtml) {
+
+     case "w":
+       var crash = new Audio('sounds/crash.mp3');
+       crash.play();
+     break;
+
+     case "a":
+       var kick_bass = new Audio('sounds/kick-bass.mp3');
+       kick_bass.play();
+     break;
+
+     case "s":
+       var snare = new Audio('sounds/snare.mp3');
+       snare.play();
+     break;
+
+     case "d":
+       var tom1 = new Audio('sounds/tom-1.mp3');
+       tom1.play();
+     break;
+
+     case "j":
+       var tom2 = new Audio('sounds/tom-2.mp3');
+       tom2.play();
+     break;
+
+     case "k":
+       var tom3 = new Audio('sounds/tom-3.mp3');
+       tom3.play();
+     break;
+
+     case "l":
+       var tom4 = new Audio('sounds/tom-4.mp3');
+       tom4.play();
+     break;
+
+     default:
+     alert("Mouse pressed at wrong position");
+     break;
+
+   }
+
+   function buttonAnimation(innerHtml) {
+
+     var activeButton = document.querySelector("." + innerHtml);
+     activeButton.classList.add("pressed");
+     setTimeout( function() {
+       activeButton.classList.remove("pressed");
+     } , 100);
+
+   }
+
+  });
+
+  }
+
+
+  document.addEventListener("keydown", function(event) {
+
+    var KeyValue = event.key;
+
+    switch (KeyValue) {
+
+      case "w":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+      break;
+
+      case "a":
+        var kick_bass = new Audio('sounds/kick-bass.mp3');
+        kick_bass.play();
+      break;
+
+      case "s":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+      break;
+
+      case "d":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      break;
+
+      case "j":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+      break;
+
+      case "k":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+      break;
+
+      case "l":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+      break;
+
+      default:
+      alert("Mouse pressed at wrong position");
+      break;
+
+    }
+
+  });
